dea61b1c56ffea4f1e1cbd7fdba6147b63c23331
sungchan1 <sungchan254@naver.com>
2025-04-18 01:20:07 +0900

    fix error root router

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/Homepage';
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+        </Routes>
+    );
+}
+
+export default App;
+
